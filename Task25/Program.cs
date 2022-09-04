5d55c40583adca9b54ec1cff71c26dad1904c78e
Dmitry <vip.mihaslw@mail>
2022-09-04 22:06:05 +0300

    HomeWork004

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на 
+// вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int Quart(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+        result = result * numA;
+    return result;
+}
+
+
+int quart = Quart(numA, numB);
+Console.WriteLine($"Число {numA} в натуральной степени {numB} = " + quart);
---
+++ Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+int[] result = FullArray(8, 0, 100);
+PrintArr(result);
+
+int[] FullArray(int n, int min, int max)
+{
+  int[] array = new int[n];
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(min, max);
+  }
+  return array;
+ }
+
+void PrintArr(int[] array)
+{
+  for (int i = 0; i<array.Length; i++)
+  {
+  Console.Write(array[i] + ","); 
+  }
+}
